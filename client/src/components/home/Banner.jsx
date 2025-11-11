aa841aa7bec90e61c21b7dbb195279c70f60414f
bhagatankit05 <bhagatankit82@gmail.com>
2025-11-11 19:42:29 +0530

    Add Banner component to display new AI feature announcement

---
+++ client/src/components/home/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className="w-full py-2.5 font-medium text-sm text-blue-100 text-center bg-gradient-to-r from-gray-900 to-gray-800 border-b border-gray-700">
+      <p>
+        <span className="px-3 py-1 rounded-lg text-black bg-yellow-400 mr-2 font-semibold">New</span>
+        AI Feature Added
+      </p>
+    </div>
+  )
+}
+
+export default Banner
